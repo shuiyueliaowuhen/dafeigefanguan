bf82afa6edc8fa9bbf165dd21de551f3f1518337
songfei <784816716@qq.com>
2021-01-26 20:51:35 +0800

    登陆

--- app/src/main/java/com/sf/diancan/ui/activity/BaseActivity.java
+++ app/src/main/java/com/sf/diancan/ui/activity/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.sf.diancan.ui.activity;
 
+import android.app.ProgressDialog;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,7 @@ import com.sf.diancan.R;
 
 public class BaseActivity extends AppCompatActivity {
 
+    private ProgressDialog mLoadingDialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,6 +21,8 @@ public class BaseActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(0xFF000000);
         }
+        mLoadingDialog = new ProgressDialog(this);
+        mLoadingDialog.setMessage("加载中...");
     }
 
     protected void setUpToolBar() {
@@ -31,5 +35,22 @@ public class BaseActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    protected void startLoadingProgress() {
+        mLoadingDialog.show();
+    }
+
+    protected void stopLoadingProgress() {
+        if (mLoadingDialog != null && mLoadingDialog.isShowing()) {
+            mLoadingDialog.dismiss();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopLoadingProgress();
+        mLoadingDialog = null;
     }
 }
--- app/src/main/java/com/sf/diancan/ui/activity/LoginActivity.java
+++ app/src/main/java/com/sf/diancan/ui/activity/LoginActivity.java
@@ -4,19 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.sf.diancan.R;
+import com.sf.diancan.UserInfoHolder;
+import com.sf.diancan.bean.User;
+import com.sf.diancan.biz.UserBiz;
+import com.sf.diancan.net.CommonCallback;
+import com.sf.diancan.utils.T;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
 
     private EditText mEtUsername;
     private EditText mEtPassword;
     private Button mBtnLogin;
     private TextView mBtnRegister;
+
+    UserBiz userBiz = new UserBiz();
+
 
     public LoginActivity() {
     }
@@ -44,7 +53,33 @@ public class LoginActivity extends AppCompatActivity {
         mBtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                toOrderActivity();
+
+                String username = mEtUsername.getText().toString();
+                String password = mEtPassword.getText().toString();
+
+                if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+                    T.showToast("账号或者密码不能为空");
+                    return;
+                }
+
+                startLoadingProgress();
+
+                userBiz.login(username, password, new CommonCallback<User>() {
+                    @Override
+                    public void onError(Exception e) {
+                        stopLoadingProgress();
+                        T.showToast(e.getMessage());
+                    }
+
+                    @Override
+                    public void onSuccess(User user) {
+                        stopLoadingProgress();
+                        T.showToast("登录成功");
+                        UserInfoHolder.getInstance().setUser(user);
+                        toOrderActivity();
+                        finish();
+                    }
+                });
             }
         });
 
